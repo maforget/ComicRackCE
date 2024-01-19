aa4ebe41cc5d9b52b3575599ddb6dd7684c77bbf
Apiweb <apiweb2007@gmail.com>
2024-01-19 08:37:03 -0300

    Improve performance when handling with tar files

--- ComicRack.Engine/IO/Provider/Readers/Archive/TarSharpZipEngine.cs
+++ ComicRack.Engine/IO/Provider/Readers/Archive/TarSharpZipEngine.cs
@@ -1,114 +1,125 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using ICSharpCode.SharpZipLib.Tar;
 
 namespace cYo.Projects.ComicRack.Engine.IO.Provider.Readers.Archive
 {
-	public class TarSharpZipEngine : FileBasedAccessor
-	{
-		public const int BufferSize = 131072;
+    public class TarSharpZipEngine : FileBasedAccessor
+    {
+        private const int BufferSize = 131072;
 
-		public TarSharpZipEngine()
-			: base(5)
-		{
-		}
+        public TarSharpZipEngine()
+            : base(5)
+        {
+        }
 
-		public override bool IsFormat(string source)
-		{
-			try
-			{
-				using (FileStream inputStream = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, 131072))
-				{
-					using (TarInputStream tarInputStream = new TarInputStream(inputStream))
-					{
-						return tarInputStream.GetNextEntry() != null;
-					}
-				}
-			}
-			catch
-			{
-				return false;
-			}
-		}
+        public override bool IsFormat(string source)
+        {
+            try
+            {
+                using (FileStream inputStream = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, BufferSize))
+                {
+                    using (TarInputStream tarInputStream = new TarInputStream(inputStream, Encoding.UTF8))
+                    {
+                        return tarInputStream.GetNextEntry() != null;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
-		public override IEnumerable<ProviderImageInfo> GetEntryList(string source)
-		{
-			using (FileStream fs = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, 131072))
-			{
-				using (TarInputStream tis = new TarInputStream(fs))
-				{
-					while (true)
-					{
-						TarEntry nextEntry = tis.GetNextEntry();
-						if (nextEntry == null)
-						{
-							break;
-						}
-						if (!nextEntry.IsDirectory)
-						{
-							yield return new ProviderImageInfo(0, nextEntry.Name, nextEntry.Size);
-						}
-					}
-				}
-			}
-		}
+        public override IEnumerable<ProviderImageInfo> GetEntryList(string source)
+        {
+            using (FileStream fs = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, BufferSize))
+            {
+                using (TarInputStream tis = new TarInputStream(fs, Encoding.UTF8))
+                {
+                    while (true)
+                    {
+                        TarEntry nextEntry = tis.GetNextEntry();
+                        if (nextEntry == null)
+                        {
+                            break;
+                        }
 
-		public override byte[] ReadByteImage(string source, ProviderImageInfo info)
-		{
-			try
-			{
-				using (FileStream inputStream = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, 131072))
-				{
-					using (TarInputStream tarInputStream = new TarInputStream(inputStream))
-					{
-						TarEntry nextEntry;
-						do
-						{
-							nextEntry = tarInputStream.GetNextEntry();
-						}
-						while (!(nextEntry.Name == info.Name));
-						byte[] array = new byte[info.Size];
-						if (tarInputStream.Read(array, 0, array.Length) != array.Length)
-						{
-							throw new IOException();
-						}
-						return array;
-					}
-				}
-			}
-			catch
-			{
-				return null;
-			}
-		}
+                        if (!nextEntry.IsDirectory)
+                        {
+                            yield return new ProviderImageInfo(0, nextEntry.Name, nextEntry.Size);
+                        }
+                    }
+                }
+            }
+        }
 
-		public override ComicInfo ReadInfo(string source)
-		{
-			try
-			{
-				using (FileStream inputStream = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, 131072))
-				{
-					using (TarInputStream tarInputStream = new TarInputStream(inputStream))
-					{
-						TarEntry nextEntry;
-						do
-						{
-							nextEntry = tarInputStream.GetNextEntry();
-						}
-						while (string.Compare(Path.GetFileName(nextEntry.Name), "ComicInfo.xml", ignoreCase: true) != 0);
-						byte[] array = new byte[nextEntry.Size];
-						tarInputStream.Read(array, 0, array.Length);
-						using (MemoryStream inStream = new MemoryStream(array))
-						{
-							return ComicInfo.Deserialize(inStream);
-						}
-					}
-				}
-			}
-			catch
-			{
-				return null;
-			}
-		}
-	}
+        public override byte[] ReadByteImage(string source, ProviderImageInfo info)
+        {
+            try
+            {
+                using (FileStream inputStream = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, BufferSize))
+                {
+                    using (TarInputStream tarInputStream = new TarInputStream(inputStream, Encoding.UTF8))
+                    {
+                        TarEntry nextEntry;
+                        do
+                        {
+                            nextEntry = tarInputStream.GetNextEntry();
+                        }
+                        while (!(nextEntry.Name == info.Name));
+                        byte[] array = new byte[info.Size];
+                        if (tarInputStream.Read(array, 0, array.Length) != array.Length)
+                        {
+                            throw new IOException();
+                        }
+                        return array;
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public override ComicInfo ReadInfo(string source)
+        {
+            try
+            {
+                byte[] buffer = new byte[BufferSize];
+
+                using (FileStream inputStream =
+                       new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read, BufferSize))
+                using (TarInputStream tarInputStream = new TarInputStream(inputStream, Encoding.UTF8))
+                {
+                    TarEntry nextEntry;
+                    do
+                    {
+                        nextEntry = tarInputStream.GetNextEntry();
+                    } while (String.Compare(Path.GetFileName(nextEntry.Name), "ComicInfo.xml",
+                                 StringComparison.OrdinalIgnoreCase) != 0);
+
+                    using (MemoryStream inStream = new MemoryStream())
+                    {
+                        int bytesRead;
+                        while ((bytesRead = tarInputStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            inStream.Write(buffer, 0, bytesRead);
+                        }
+
+                        inStream.Position = 0;
+
+                        return ComicInfo.Deserialize(inStream);
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
 }
