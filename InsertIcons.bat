74f87a4bf17d948771653c616efb06323c939c7a
maforget <11904426+maforget@users.noreply.github.com>
2024-01-21 18:40:49 -0500

    Alternate way to insert icons

--- InsertIcons.bat
+++
@@ -1,6 +0,0 @@
-:: InsertIcons.exe "%~dp0\ComicRack\bin\Release\ComicRack.exe" "%~dp0\ComicRack\Icons"
-InsertIcons.exe "%~dp0\ComicRack\bin\Release\ComicRack.exe" "%~dp0\ComicRack\Icons\icon1.ico";"%~dp0\ComicRack\Icons\icon2.ico";"%~dp0\ComicRack\Icons\icon3.ico";"%~dp0\ComicRack\Icons\icon4.ico"
-
-:: copy /Y "%~dp0\ComicRack\bin\Release\ComicRack.exe" E:\
-:: InsertIcons.exe E:\ComicRack.exe "%~dp0\ComicRack\Icons"
-:: copy /Y "E:\ComicRack.exe" "%~dp0\ComicRack\bin\Release\ComicRack.exe"
