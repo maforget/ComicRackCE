7963494503c9f0ed5a2dd5e04ba15277d0dc3a77
maforget <11904426+maforget@users.noreply.github.com>
2024-01-21 21:54:33 -0500

    Replaced res by InsertIcons

---
+++ InsertIcons.bat
@@ -0,0 +1,6 @@
+:: InsertIcons.exe "%~dp0\ComicRack\bin\Release\ComicRack.exe" "%~dp0\ComicRack\Icons"
+InsertIcons.exe "%~dp0\ComicRack\bin\Release\ComicRack.exe" "%~dp0\ComicRack\Icons\icon1.ico";"%~dp0\ComicRack\Icons\icon2.ico";"%~dp0\ComicRack\Icons\icon3.ico";"%~dp0\ComicRack\Icons\icon4.ico"
+
+:: copy /Y "%~dp0\ComicRack\bin\Release\ComicRack.exe" E:\
+:: InsertIcons.exe E:\ComicRack.exe "%~dp0\ComicRack\Icons"
+:: copy /Y "E:\ComicRack.exe" "%~dp0\ComicRack\bin\Release\ComicRack.exe"
