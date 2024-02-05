db71f5a2add0a7c3808b49a4f7dc24865d0980b3
maforget <11904426+maforget@users.noreply.github.com>
2024-02-04 19:30:29 -0500

    Added an AtomNewsFeed

---
+++ cYo.Common/Net/News/Atom.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace cYo.Common.Net.News
+{
+    public static class Atom
+    {
+        public static string TrimEndLine(this string text)
+        {
+            return text.Trim('\n').Trim();
+        }
+
+        [Serializable()]
+        [XmlType(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
+        [XmlRoot(Namespace = "http://www.w3.org/2005/Atom", IsNullable = false)]
+        public class feed
+        {
+            [XmlElement("entry")]
+            public List<feedEntry> entries { get; set; }
+
+            public string id { get; set; }
+
+            [XmlElement("link")]
+            public List<feedLink> links { get; set; }
+
+            public string title { get; set; }
+
+            public DateTime updated { get; set; }
+        }
+
+        [Serializable()]
+        [XmlType(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
+        public class feedEntry
+        {
+            private string _title;
+
+            public feedEntryAuthor author { get; set; }
+            public feedEntryContent content { get; set; }
+            public string id { get; set; }
+
+            public feedEntryLink link { get; set; }
+
+            [XmlElement(Namespace = "http://search.yahoo.com/mrss/")]
+            public thumbnail thumbnail { get; set; }
+
+            public string title { get => _title.TrimEndLine(); set => _title = value; }
+
+            public DateTime updated { get; set; }
+        }
+
+        [Serializable()]
+        [XmlType(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
+        public class feedEntryAuthor
+        {
+            public string name { get; set; }
+
+            public string uri { get; set; }
+        }
+
+        [Serializable()]
+        [XmlType(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
+        public class feedEntryContent
+        {
+            private string _value;
+
+            [XmlAttribute()]
+            public string type { get; set; }
+
+            [XmlText()]
+            public string Value { get => _value.TrimEndLine(); set => _value = value; }
+        }
+
+        [XmlType(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
+        public class feedEntryLink
+        {
+            [XmlAttribute()]
+            public string href { get; set; }
+
+            [XmlAttribute()]
+            public string rel { get; set; }
+
+            [XmlAttribute()]
+            public string type { get; set; }
+        }
+
+        [Serializable()]
+        [XmlType(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
+        public class feedLink
+        {
+            [XmlAttribute()]
+            public string href { get; set; }
+
+            [XmlAttribute()]
+            public string rel { get; set; }
+
+            [XmlAttribute()]
+            public string type { get; set; }
+        }
+
+        [Serializable()]
+        [XmlType(AnonymousType = true, Namespace = "http://search.yahoo.com/mrss/")]
+        [XmlRoot(Namespace = "http://search.yahoo.com/mrss/", IsNullable = false)]
+        public class thumbnail
+        {
+            [XmlAttribute()]
+            public byte height { get; set; }
+
+            [XmlAttribute()]
+            public string url { get; set; }
+
+            [XmlAttribute()]
+            public byte width { get; set; }
+        }
+    }
+}
---
+++ cYo.Common/Net/News/AtomNewsFeed.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Linq;
+using cYo.Common.Xml;
+
+namespace cYo.Common.Net.News
+{
+    public class AtomNewsFeed : NewsFeed
+    {
+        protected override NewsChannelCollection ParseFeed(string xmlFeed)
+        {
+            NewsChannelCollection newsChannelCollection = new NewsChannelCollection();
+            var feed = XmlUtility.FromString<Atom.feed>(xmlFeed);
+
+            NewsChannel newsChannel = new NewsChannel();
+            newsChannel.Title = feed.title;
+            newsChannel.Link = feed.links.FirstOrDefault(x => x.rel == "alternate")?.href;
+
+            foreach (Atom.feedEntry item in feed.entries.OrderByDescending(dt => dt.updated))
+            {
+                NewsChannelItem newsChannelItem = new NewsChannelItem()
+                {
+                    Author = item.author.name,
+                    Title = item.title,
+                    Description = item.title,
+                    Guid = item.id,
+                    Link = item.link.href,
+                    Published = item.updated,
+                    Category = "Commit"
+                };
+
+                newsChannel.Items.Add(newsChannelItem);
+
+            }
+
+            newsChannelCollection.Add(newsChannel);
+            return newsChannelCollection;
+        }
+
+
+    }
+}
