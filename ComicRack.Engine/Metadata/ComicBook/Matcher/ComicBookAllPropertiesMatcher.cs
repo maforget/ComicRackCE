3801ad0efe5607f71f45e07c8af75983b5c6e637
maforget <11904426+maforget@users.noreply.github.com>
2024-03-26 14:49:23 -0400

    [skip ci] updated change.txt

--- ComicRack.Engine/Metadata/ComicBook/Matcher/ComicBookAllPropertiesMatcher.cs
+++ ComicRack.Engine/Metadata/ComicBook/Matcher/ComicBookAllPropertiesMatcher.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using cYo.Common.Collections;
 using cYo.Common.ComponentModel;
 
 namespace cYo.Projects.ComicRack.Engine
