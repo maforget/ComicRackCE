faa5fae4fac5023071e45c0f486aed740811ee69
maforget <11904426+maforget@users.noreply.github.com>
2024-05-20 15:23:58 -0400

    [skip ci] Fix spelling of Thumbnail

--- ComicRack.Engine/IO/Provider/Readers/ComicProvider.cs
+++ ComicRack.Engine/IO/Provider/Readers/ComicProvider.cs
@@ -88,14 +88,14 @@ namespace cYo.Projects.ComicRack.Engine.IO.Provider.Readers
 
 		protected virtual bool IsSupportedImage(ProviderImageInfo file)
         {
-            if(IsImageThumnailFolder(file.Name) || IsFileTooSmall(file.Size))
+            if(IsImageThumbnailFolder(file.Name) || IsFileTooSmall(file.Size))
 				return false;
 
             string fileExt = Path.GetExtension(FileUtility.MakeValidFilename(file.Name));
             return supportedTypes.Any((string ext) => string.Equals(fileExt, "." + ext, StringComparison.OrdinalIgnoreCase));
         }
 
-        private static bool IsImageThumnailFolder(string file)
+        private static bool IsImageThumbnailFolder(string file)
         {
             string[] ignore = { ".DS_Store\\", "__MACOSX\\" };
             return ignore.Any(item => file.Contains(item));
