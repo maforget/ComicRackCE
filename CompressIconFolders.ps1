9836be805cfb6a47335ae0bc52f90b946f165b33
maforget <11904426+maforget@users.noreply.github.com>
2024-07-04 21:52:27 -0400

    Skip creating Resources/Icons zip if nothing has changed
    Speeds up building when not required (git is required)

--- CompressIconFolders.ps1
+++ CompressIconFolders.ps1
@@ -27,6 +27,9 @@ if (-not (Test-Path $iconDirectory)) {
     exit 1
 }
 
+# Use Git to check for changes in the Resources/Icons directory
+$gitChangedFiles = git status --porcelain | Select-String -Pattern "^.+/Resources/Icons/"
+
 $folders = Get-ChildItem -Path $iconDirectory -Directory
 
 # Iterate through each folder
@@ -34,16 +37,40 @@ foreach ($folder in $folders) {
     # Define the full path to the folder and the destination zip file
     $folderPath = Join-Path -Path $folder.FullName -ChildPath "*.*"
     $destinationPath = "$($folder.FullName).zip"
-    
-    # Compress the folder into a zip file
-    Compress-Archive -Path $folderPath -DestinationPath $destinationPath -Force
 
+    # Check if the zip file exists
+    $zipExists = Test-Path $destinationPath
+
+    # Determine if we need to update the zip file
+    $updateRequired = $false
+    if ($zipExists) {
+        if ($gitChangedFiles.Count -gt 0) {
+            foreach ($changedFile in $gitChangedFiles) {
+                if ($changedFile -like "*$($folder.Name)/*") {
+                    $updateRequired = $true
+                    Write-Output "$destinationPath exists, but there are changes, updating it."
+                    break
+                }
+            }
+        }
+    } else {
+        $updateRequired = $true
+        Write-Output "$destinationPath doesn't exists, need to create it."
+    }
+    
+    if ($updateRequired) {
+        # Compress the folder into a zip file
+        Compress-Archive -Path $folderPath -DestinationPath $destinationPath -Force
+        } else {
+        Write-Output "$destinationPath is up to date. No action required."
+    }
+
+  
     # Check if the zip file was created successfully
     if (Test-Path $destinationPath) {
         # Delete the original folder if the zip file exists
         Remove-Item -Path $folder.FullName -Recurse -Force
     } else {
         Write-Error "Failed to create $destinationPath"
-        exit 1
-    }
+    } 
 }
