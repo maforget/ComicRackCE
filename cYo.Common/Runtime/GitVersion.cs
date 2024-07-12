8a8a7b271a674941763608d8c8800fc1d851f76c
maforget <11904426+maforget@users.noreply.github.com>
2024-07-12 17:18:47 -0400

    Moved git info to the Main executable, so it is updated on each build.

--- cYo.Common/Runtime/GitVersion.cs
+++ cYo.Common/Runtime/GitVersion.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace cYo.Common.Runtime
 {
@@ -12,10 +14,33 @@ namespace cYo.Common.Runtime
     {
         public static string GetCurrentVersionInfo()
         {
-            var isDirty = string.IsNullOrEmpty(Properties.Resources.isDirty?.Trim()) ? "" : "-dirty";
-            var currentCommit = string.IsNullOrEmpty(Properties.Resources.CurrentCommit) ? "" : $" [{Properties.Resources.CurrentCommit?[..7]}{isDirty}]";
+            Assembly assembly = Assembly.GetEntryAssembly();
+            var isDirty = string.IsNullOrEmpty(GetString("isDirty", assembly)?.Trim()) ? "" : "-dirty";
+            string currentCommit = GetString("CurrentCommit", assembly);
+            return string.IsNullOrEmpty(currentCommit) ? "" : $" [{currentCommit[..7]}{isDirty}]";
+        }
 
-            return currentCommit;
+        public static Stream GetStream(string resourceName, Assembly assembly)
+        {
+            string fullResourceName = assembly.GetManifestResourceNames().FirstOrDefault(x => x.Contains(resourceName));
+            return assembly.GetManifestResourceStream(fullResourceName); ;
+        }
+
+
+        public static string GetString(string resourceName, Assembly containingAssembly)
+        {
+            string result = String.Empty;
+            Stream sourceStream = GetStream(resourceName, containingAssembly);
+
+            if (sourceStream != null)
+            {
+                using (StreamReader streamReader = new StreamReader(sourceStream))
+                {
+                    result = streamReader.ReadToEnd();
+                }
+            }
+
+            return result;
         }
     }
 }
