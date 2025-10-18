19ba90ee4a45d5f9ffaf44520989ba68e667da13
gman-comicdb <218506606+gman-comicdb@users.noreply.github.com>
2025-10-18 10:01:29 +0100

    DarkControlPaint - v1. To handle `VisualStyleRenderer` and `ControlPaint` not using `SystemColorsEx`. (and `VisualStyleRenderer` sometimes using light mode uxtheme)
    [partial change; unlikely to compile]

---
+++ cYo.Common.Windows/Forms/Theme/DarkMode/Rendering/DarkControlPaint.cs
@@ -0,0 +1,74 @@
+﻿using cYo.Common.Windows.Forms.Theme.DarkMode.Resources;
+using cYo.Common.Windows.Forms.Theme.Resources;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace cYo.Common.Windows.Forms.Theme.DarkMode.Rendering;
+
+internal static class DarkControlPaint
+{
+
+    #region DrawBorder
+    internal static void DrawBorder(Graphics g, Rectangle bounds, Border3DStyle borderStyle) => DrawBorder(g, bounds);
+
+    internal static void DrawBorder(Graphics g, Rectangle bounds)
+    {
+        ControlPaint.DrawBorder(g, bounds, DarkColors.Border.Default, ButtonBorderStyle.Solid);
+    }
+    #endregion
+
+    #region DrawFocusRectangle
+    internal static void DrawFocusRectangle(DrawItemEventArgs e)
+    {
+        if ((e.State & DrawItemState.Focus) == DrawItemState.Focus && (e.State & DrawItemState.NoFocusRect) != DrawItemState.NoFocusRect)
+        {
+            //ControlPaint.DrawFocusRectangle(e.Graphics, e.Bounds, e.ForeColor, e.BackColor);
+            ControlPaint.DrawFocusRectangle(e.Graphics, e.Bounds, Color.Red, Color.Cyan);
+        }
+    }
+
+    public static void DrawFocusRectangle(Graphics graphics, Rectangle rectangle)
+    {
+        DrawFocusRectangle(graphics, rectangle, SystemColors.ControlText, SystemColors.Control);
+    }
+
+    public static void DrawFocusRectangle(Graphics graphics, Rectangle rectangle, Color foreColor, Color backColor)
+    {
+        DrawFocusRectangle(graphics, rectangle, backColor, highContrast: false);
+    }
+
+    private static void DrawFocusRectangle(Graphics graphics, Rectangle rectangle, Color color, bool highContrast)
+    {
+        rectangle.Width--;
+        rectangle.Height--;
+        graphics.DrawRectangle(DarkPens.SelectedText.Focus, rectangle);
+    }
+    #endregion
+
+    #region DrawBackground
+    internal static void DrawBackground(PaintEventArgs e, Color backColor)
+    {
+        //e.Graphics.Clear(backColor); // emulating non-VisualStyleRenderer path for now
+        e.Graphics.Clear(backColor);
+    }
+
+    internal static void DrawBackground(DrawItemEventArgs e)
+    {
+        //e.Graphics.Clear(backColor); // emulating non-VisualStyleRenderer path for now
+        //e.DrawThemeBackground();
+        e.DrawBackground();
+        //if (e.State.HasFlag(DrawItemState.Selected)) 
+        //    e.DrawThemeFocusRectangle();
+    }
+
+    internal static void DrawBackground(DrawToolTipEventArgs e)
+    {
+        e.Graphics.FillRectangle(new SolidBrush(ThemeColors.ToolTip.Back), new Rectangle(Point.Empty, e.Bounds.Size));
+    }
+
+    internal static void DrawBorder(DrawToolTipEventArgs e)
+    {
+
+    }
+    #endregion
+}
