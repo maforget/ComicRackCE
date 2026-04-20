83646a5f145cad2de245a8a66a594ef49642e456
maforget <11904426+maforget@users.noreply.github.com>
2026-04-19 23:05:20 -0400

    BUGFIX: Paste dialog not remembering custom values & Refactor ComicDataPasteDialog for clarity and maintainability
    - Reorganized logic, improved variable names, and added XML documentation to public methods.
    - Adjusted checkbox initialization order so previous custom values are checked.
    - Clarified enabling/visibility logic.

--- ComicRack/Dialogs/ComicDataPasteDialog.cs
+++ ComicRack/Dialogs/ComicDataPasteDialog.cs
@@ -15,181 +15,190 @@ using cYo.Projects.ComicRack.Viewer.Config;
 
 namespace cYo.Projects.ComicRack.Viewer.Dialogs
 {
-	public partial class ComicDataPasteDialog : FormEx
-	{
-		public ComicDataPasteDialog()
-		{
-			LocalizeUtility.UpdateRightToLeft(this);
-			InitializeComponent();
-			this.RestorePosition();
-			this.RestorePanelStates();
-			LocalizeUtility.Localize(this, null);
-		}
+    public partial class ComicDataPasteDialog : FormEx
+    {
+        public ComicDataPasteDialog()
+        {
+            LocalizeUtility.UpdateRightToLeft(this);
+            InitializeComponent();
+            this.RestorePosition();
+            this.RestorePanelStates();
+            LocalizeUtility.Localize(this, null);
+        }
 
-		protected override void OnLoad(EventArgs e)
-		{
-			base.OnLoad(e);
-			FormUtility.RegisterPanelToTabToggle(pageData, PropertyCaller.CreateFlagsValueStore(Program.Settings, "TabLayouts", TabLayouts.Paste));
-		}
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            FormUtility.RegisterPanelToTabToggle(pageData, PropertyCaller.CreateFlagsValueStore(Program.Settings, "TabLayouts", TabLayouts.Paste));
+        }
 
-		public void SetChecks(ComicBook data, IEnumerable<string> properties)
-		{
-			grpCatalog.Visible = !data.IsLinked || !Program.Settings.CatalogOnlyForFileless;
-			foreach (CheckBox control in this.GetControls<CheckBox>())
-			{
-				string text = control.Tag as string;
-				if (properties.Contains(text))
-				{
-					control.Checked = true;
-				}
-				try
-				{
-					if (!data.IsDefaultValue(text))
-					{
-						control.Font = FC.Get(control.Font, FontStyle.Bold);
-					}
-				}
-				catch (Exception)
-				{
-				}
-			}
-		}
+        /// <summary>
+        /// Sets the checkboxes based on the properties that were pasted last time. The Tag property of each checkbox is used as the property name. If a property has a non-default value, its checkbox is shown in bold.
+        /// </summary>
+        public void SetChecks(ComicBook data, IEnumerable<string> properties)
+        {
+            grpCatalog.Visible = !data.IsLinked || !Program.Settings.CatalogOnlyForFileless;
+            foreach (CheckBox control in this.GetControls<CheckBox>())
+            {
+                string text = control.Tag as string;
+                if (properties.Contains(text))
+                    control.Checked = true;
 
-		public IEnumerable<string> GetChecks()
-		{
-			return (from cb in this.GetControls<CheckBox>()
-				where cb.Enabled
-				where cb.Checked && cb.Tag != null
-				select (string)cb.Tag).ToArray();
-		}
+                try
+                {
+                    if (!data.IsDefaultValue(text))
+                        control.Font = FC.Get(control.Font, FontStyle.Bold);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
 
-		private void btMarkDefined_Click(object sender, EventArgs e)
-		{
-			foreach (CheckBox control in this.GetControls<CheckBox>())
-			{
-				control.Checked = control.Font.Bold;
-			}
-		}
+        /// <summary>
+        /// Returns the list of properties to paste, based on the enabled/checked checkboxes. The Tag property of each checkbox is used as the property name.
+        /// </summary>
+        public IEnumerable<string> GetChecks()
+        {
+            return (from cb in this.GetControls<CheckBox>()
+                    where cb.Enabled
+                    where cb.Checked && cb.Tag != null
+                    select (string)cb.Tag).ToArray();
+        }
 
-		private void btMarkAll_Click(object sender, EventArgs e)
-		{
-			foreach (CheckBox control in this.GetControls<CheckBox>())
-			{
-				control.Checked = true;
-			}
-		}
+        private void btMarkDefined_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox control in this.GetControls<CheckBox>())
+            {
+                control.Checked = control.Font.Bold;
+            }
+        }
 
-		private void btMarkNone_Click(object sender, EventArgs e)
-		{
-			foreach (CheckBox control in this.GetControls<CheckBox>())
-			{
-				control.Checked = false;
-			}
-		}
+        private void btMarkAll_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox control in this.GetControls<CheckBox>())
+            {
+                control.Checked = true;
+            }
+        }
 
-		public static void ShowAndPaste(IWin32Window parent, ComicBook data, IEnumerable<ComicBook> books)
-		{
-			IEnumerable<string> checks = null;
-			int count = books.Count();
-			string[] array = (Program.Settings.ShowCustomBookFields ? Program.Database.CustomValues.Where((string k) => Program.ExtendedSettings.ShowCustomScriptValues || !k.Contains('.')).ToArray() : null);
-			using (ComicDataPasteDialog comicDataPasteDialog = new ComicDataPasteDialog())
-			{
-				comicDataPasteDialog.SetChecks(data, Program.Settings.PasteProperties.Split(';'));
-				comicDataPasteDialog.Text = StringUtility.Format(comicDataPasteDialog.Text, count);
-				if (array == null || array.Length == 0)
-				{
-					comicDataPasteDialog.grpCustom.Visible = false;
-				}
-				else
-				{
-					int num = 0;
-					Control control = null;
-					string[] array2 = array;
-					foreach (string text in array2)
-					{
-						CheckBox checkBox = (CheckBox)comicDataPasteDialog.chkBookStore.Clone();
-						comicDataPasteDialog.grpCustom.Controls.Add(checkBox);
-						checkBox.Top = comicDataPasteDialog.chkBookStore.Top + (comicDataPasteDialog.chkBookCondition.Top - comicDataPasteDialog.chkBookStore.Top) * (num / 4);
-						switch (num % 4)
-						{
-						case 0:
-							checkBox.Left = comicDataPasteDialog.chkBookStore.Left;
-							break;
-						case 1:
-							checkBox.Left = comicDataPasteDialog.chkBookPrice.Left;
-							break;
-						case 2:
-							checkBox.Left = comicDataPasteDialog.chkBookLocation.Left;
-							break;
-						case 3:
-							checkBox.Left = comicDataPasteDialog.chkBookAge.Left;
-							break;
-						}
-						checkBox.Visible = true;
-						if (data.GetCustomValue(text) != null)
-						{
-							checkBox.Font = FC.Get(checkBox.Font, FontStyle.Bold);
-						}
-						checkBox.Text = text;
-						checkBox.Tag = "{" + text + "}";
-						num++;
-						control = checkBox;
-					}
-					if (control != null)
-					{
-						comicDataPasteDialog.grpCustom.Height = control.Bottom + 8;
-					}
-				}
-				if (!books.Any((ComicBook cb) => cb.IsLinked))
-				{
-					comicDataPasteDialog.chkScanInformation.Visible = false;
-				}
-				else
-				{
-					CheckBox checkBox2 = comicDataPasteDialog.chkOpenedTime;
-					CheckBox checkBox3 = comicDataPasteDialog.chkPageCount;
-					CheckBox checkBox4 = comicDataPasteDialog.chkOpenedTime;
-					bool flag2 = (comicDataPasteDialog.chkPageCount.Enabled = false);
-					bool flag4 = (checkBox4.Enabled = flag2);
-					bool visible = (checkBox3.Visible = flag4);
-					checkBox2.Visible = visible;
-				}
-				if (!books.Any((ComicBook cb) => cb.IsInContainer))
-				{
-					CheckBox checkBox5 = comicDataPasteDialog.chkCommunityRating;
-					CheckBox checkBox6 = comicDataPasteDialog.chkRating;
-					CheckBox checkBox7 = comicDataPasteDialog.chkTags;
-					CheckBox checkBox8 = comicDataPasteDialog.chkColor;
-					bool flag7 = (comicDataPasteDialog.chkSeriesComplete.Enabled = false);
-					bool flag2 = (checkBox8.Enabled = flag7);
-					bool flag4 = (checkBox7.Enabled = flag2);
-					bool visible = (checkBox6.Enabled = flag4);
-					checkBox5.Enabled = visible;
-				}
-				if (comicDataPasteDialog.ShowDialog(parent) == DialogResult.OK)
-				{
-					checks = comicDataPasteDialog.GetChecks();
-				}
-				Program.Settings.PasteProperties = comicDataPasteDialog.GetChecks().ToListString(";");
-			}
-			if (checks == null)
-			{
-				return;
-			}
-			AutomaticProgressDialog.Process(parent, TR.Messages["WriteData", "Pasting Data"], TR.Messages["WriteDataText", "Pasting Data into selected Books"], 1000, delegate
-			{
-				int num2 = 0;
-				foreach (ComicBook book in books)
-				{
-					if (AutomaticProgressDialog.ShouldAbort)
-					{
-						break;
-					}
-					book.RefreshInfoFromFile(RefreshInfoOptions.None);
-					book.CopyDataFrom(data, checks);
-					AutomaticProgressDialog.Value = num2++ * 100 / count;
-				}
-			}, AutomaticProgressDialogOptions.EnableCancel);
-		}
-	}
+        private void btMarkNone_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox control in this.GetControls<CheckBox>())
+            {
+                control.Checked = false;
+            }
+        }
+
+        /// <summary>
+        /// Shows the dialog and pastes the data into the books if the user clicks OK.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static void ShowAndPaste(IWin32Window parent, ComicBook data, IEnumerable<ComicBook> books)
+        {
+            IEnumerable<string> checks = null;
+            int count = books.Count();
+
+            using (ComicDataPasteDialog comicDataPasteDialog = new ComicDataPasteDialog())
+            {
+                comicDataPasteDialog.Text = StringUtility.Format(comicDataPasteDialog.Text, count); // Set the number of books in the Title
+
+                // Add checkboxes for custom values
+                string[] customValues = Program.Settings.ShowCustomBookFields ? Program.Database.CustomValues.Where((string k) => Program.ExtendedSettings.ShowCustomScriptValues || !k.Contains('.')).ToArray() : null;
+                if (customValues is not null && customValues.Length != 0) // Add checkboxes for custom values
+                {
+                    int num = 0;
+                    Control control = null;
+                    foreach (string text in customValues)
+                    {
+                        CheckBox checkBox = (CheckBox)comicDataPasteDialog.chkBookStore.Clone();
+                        comicDataPasteDialog.grpCustom.Controls.Add(checkBox);
+                        checkBox.Top = comicDataPasteDialog.chkBookStore.Top + (comicDataPasteDialog.chkBookCondition.Top - comicDataPasteDialog.chkBookStore.Top) * (num / 4);
+                        switch (num % 4)
+                        {
+                            case 0:
+                                checkBox.Left = comicDataPasteDialog.chkBookStore.Left;
+                                break;
+                            case 1:
+                                checkBox.Left = comicDataPasteDialog.chkBookPrice.Left;
+                                break;
+                            case 2:
+                                checkBox.Left = comicDataPasteDialog.chkBookLocation.Left;
+                                break;
+                            case 3:
+                                checkBox.Left = comicDataPasteDialog.chkBookAge.Left;
+                                break;
+                        }
+                        checkBox.Visible = true;
+                        if (data.GetCustomValue(text) != null)
+                            checkBox.Font = FC.Get(checkBox.Font, FontStyle.Bold);
+
+                        checkBox.Text = text;
+                        checkBox.Tag = "{" + text + "}";
+                        num++;
+                        control = checkBox;
+                    }
+
+                    if (control != null)
+                        comicDataPasteDialog.grpCustom.Height = control.Bottom + 8;
+                }
+                else
+                {
+                    comicDataPasteDialog.grpCustom.Visible = false;
+                }
+
+                // Set the checkboxes based on the properties that were pasted last time
+                comicDataPasteDialog.SetChecks(data, Program.Settings.PasteProperties.Split(';'));
+
+                // If there are no fileless books, hide the "Scan Information" checkbox. If there are fileless books, disable the "Opened Time" and "Page Count" checkboxes, since these values are not relevant for fileless books.
+                if (!books.Any((ComicBook cb) => cb.IsLinked))
+                {
+                    comicDataPasteDialog.chkScanInformation.Visible = false;
+                }
+                else
+                {
+                    CheckBox openedTime = comicDataPasteDialog.chkOpenedTime;
+                    CheckBox pageCount = comicDataPasteDialog.chkPageCount;
+                    openedTime.Visible = pageCount.Visible = openedTime.Enabled = pageCount.Enabled = false;
+                }
+
+                // If books aren't in the library, disable the "Community Rating", "Rating", "Tags", "Color" and "Series Complete" checkboxes.
+                if (!books.Any((ComicBook cb) => cb.IsInContainer))
+                {
+                    CheckBox communityRarting = comicDataPasteDialog.chkCommunityRating;
+                    CheckBox rating = comicDataPasteDialog.chkRating;
+                    CheckBox tags = comicDataPasteDialog.chkTags;
+                    CheckBox color = comicDataPasteDialog.chkColor;
+                    CheckBox chkSeriesComplete = comicDataPasteDialog.chkSeriesComplete;
+                    communityRarting.Enabled = rating.Enabled = tags.Enabled = color.Enabled = chkSeriesComplete.Enabled = false;
+                }
+
+                // Show the dialog and get the list of properties to paste if the user clicks OK
+                if (comicDataPasteDialog.ShowDialog(parent) == DialogResult.OK)
+                    checks = comicDataPasteDialog.GetChecks();
+
+                // Save the list of properties to paste for the next time
+                Program.Settings.PasteProperties = comicDataPasteDialog.GetChecks().ToListString(";");
+            }
+
+            // Return if there is nothing to paste
+            if (checks == null)
+                return;
+
+            // Paste the data into the books, showing a progress dialog
+            AutomaticProgressDialog.Process(parent, TR.Messages["WriteData", "Pasting Data"], TR.Messages["WriteDataText", "Pasting Data into selected Books"], 1000, () =>
+            {
+                int num2 = 0;
+                foreach (ComicBook book in books)
+                {
+                    if (AutomaticProgressDialog.ShouldAbort)
+                        break;
+
+                    book.RefreshInfoFromFile(RefreshInfoOptions.None);
+                    book.CopyDataFrom(data, checks);
+                    AutomaticProgressDialog.Value = num2++ * 100 / count;
+                }
+            }, AutomaticProgressDialogOptions.EnableCancel);
+        }
+    }
 }
