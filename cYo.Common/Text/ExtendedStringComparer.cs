fbdccd925d16aacf753ca9a032e7ba9ddec104fe
maforget <11904426+maforget@users.noreply.github.com>
2026-05-14 00:05:55 -0400

    Refactor and clarify string comparison logic
    - Refactored Compare and CompareNumbers methods in ExtendedStringComparer.cs for improved readability and correctness.

--- cYo.Common/Text/ExtendedStringComparer.cs
+++ cYo.Common/Text/ExtendedStringComparer.cs
@@ -54,179 +54,164 @@ namespace cYo.Common.Text
 		public static int Compare(string s1, string s2, ExtendedStringComparison compareMode)
 		{
 			if (string.IsNullOrEmpty(s1))
-			{
-				if (!string.IsNullOrEmpty(s2))
-				{
-					return -1;
-				}
-				return 0;
-			}
-			if (string.IsNullOrEmpty(s2))
-			{
+                return !string.IsNullOrEmpty(s2) ? -1 : 0;
+
+            if (string.IsNullOrEmpty(s2))
 				return 1;
-			}
+
 			if (s1 == s2)
-			{
 				return 0;
-			}
-			int i = 0;
+
+			int i1 = 0;
 			int i2 = 0;
 			bool zeroesFirst = (compareMode & ExtendedStringComparison.ZeroesFirst) != 0;
 			bool ignoreCase = (compareMode & ExtendedStringComparison.IgnoreCase) != 0;
-			bool flag = (compareMode & ExtendedStringComparison.Ordinal) != 0;
-			bool flag2 = (compareMode & ExtendedStringComparison.IgnoreArticles) != 0;
+			bool ordinal = (compareMode & ExtendedStringComparison.Ordinal) != 0;
+			bool ignoreArticles = (compareMode & ExtendedStringComparison.IgnoreArticles) != 0;
 			int result = 0;
-			if (flag2)
+
+			if (ignoreArticles)
 			{
-				i = s1.IndexAfterArticle();
+				i1 = s1.IndexAfterArticle();
 				i2 = s2.IndexAfterArticle();
-				result = Math.Sign(i2 - i);
-			}
+				result = Math.Sign(i2 - i1);
+            }
+
 			int length = s1.Length;
 			int length2 = s2.Length;
-			bool flag3 = char.IsLetterOrDigit(s1[i]);
-			bool flag4 = char.IsLetterOrDigit(s2[i2]);
-			if (flag3 && !flag4)
+			bool isLetterOrDigit_i1 = char.IsLetterOrDigit(s1[i1]);
+			bool isLetterOrDigit_i2 = char.IsLetterOrDigit(s2[i2]);
+
+            if (isLetterOrDigit_i1 && !isLetterOrDigit_i2)
+                return 1;
+
+            if (!isLetterOrDigit_i1 && isLetterOrDigit_i2)
+                return -1;
+
+            do
 			{
-				return 1;
-			}
-			if (!flag3 && flag4)
-			{
-				return -1;
-			}
-			do
-			{
-				char c = s1[i];
+				char c1 = s1[i1];
 				char c2 = s2[i2];
-				flag3 = char.IsDigit(c);
-				flag4 = char.IsDigit(c2);
-				if (!flag3 && !flag4)
+                bool isDigit_c1 = char.IsDigit(c1);
+                bool isDigit_c2 = char.IsDigit(c2);
+
+                if (!isDigit_c1 && !isDigit_c2)
 				{
-					if (c != c2)
+					if (c1 != c2)
 					{
-						bool flag5 = char.IsLetter(c);
-						bool flag6 = char.IsLetter(c2);
-						if (flag5 && flag6)
+						bool isLetter_c1 = char.IsLetter(c1);
+						bool isLetter_c2 = char.IsLetter(c2);
+						if (isLetter_c1 && isLetter_c2)
 						{
-							int num = (flag ? (char.ToUpper(c) - char.ToUpper(c2)) : string.Compare(s1, i, s2, i2, 1, ignoreCase));
+							int num = ordinal 
+								? (char.ToUpper(c1) - char.ToUpper(c2)) 
+								: string.Compare(s1, i1, s2, i2, 1, ignoreCase);
+
 							if (num != 0)
-							{
 								return num;
-							}
 						}
 						else
 						{
-							if (flag5 || flag6)
-							{
-								if (flag5)
-								{
-									return 1;
-								}
-								return -1;
-							}
-							int num2 = (flag ? (c - c2) : string.Compare(s1, i, s2, i2, 1));
+                            if (isLetter_c1 || isLetter_c2)
+                                return isLetter_c1 ? 1 : -1;
+
+                            int num2 = ordinal
+								? (c1 - c2) 
+								: string.Compare(s1, i1, s2, i2, 1);
+
 							if (num2 != 0)
-							{
 								return num2;
-							}
 						}
 					}
 				}
 				else
 				{
-					if (!(flag3 && flag4))
-					{
-						if (flag3)
-						{
-							return -1;
-						}
-						return 1;
-					}
-					int num3 = CompareNumbers(s1, length, ref i, s2, length2, ref i2, zeroesFirst);
-					if (num3 != 0)
-					{
-						return num3;
-					}
-				}
-				i++;
+                    if (!(isDigit_c1 && isDigit_c2))
+                        return isDigit_c1 ? -1 : 1;
+
+                    int num3 = CompareNumbers(s1, length, ref i1, s2, length2, ref i2, zeroesFirst);
+                    if (num3 != 0)
+                        return num3;
+                }
+
+				i1++;
 				i2++;
-				if (i >= length)
-				{
-					if (i2 >= length2)
-					{
-						return result;
-					}
-					return -1;
-				}
-			}
+
+                if (i1 >= length)
+                    return i2 >= length2 ? result : -1;
+            }
 			while (i2 < length2);
 			return 1;
 		}
 
 		private static int CompareNumbers(string s1, int s1Length, ref int i1, string s2, int s2Length, ref int i2, bool zeroesFirst)
 		{
-			int nzStart = i1;
+			int nzStart1 = i1;
 			int nzStart2 = i2;
-			int end = i1;
+			int end1 = i1;
 			int end2 = i2;
-			ScanNumber(s1, s1Length, i1, ref nzStart, ref end);
+			ScanNumber(s1, s1Length, i1, ref nzStart1, ref end1);
 			ScanNumber(s2, s2Length, i2, ref nzStart2, ref end2);
-			int num = i1;
-			i1 = end - 1;
-			int num2 = i2;
+
+			int start1 = i1;
+			int start2 = i2;
+
+            i1 = end1 - 1;
 			i2 = end2 - 1;
+
 			if (zeroesFirst)
 			{
-				int num3 = nzStart - num;
-				int num4 = nzStart2 - num2;
-				if (num3 > num4)
-				{
+                int leadingZeroCount1 = nzStart1 - start1;
+				int leadingZeroCount2 = nzStart2 - start2;
+
+				if (leadingZeroCount1 > leadingZeroCount2)
 					return -1;
-				}
-				if (num3 < num4)
-				{
+				if (leadingZeroCount1 < leadingZeroCount2)
 					return 1;
-				}
 			}
-			int num5 = end2 - nzStart2;
-			int num6 = end - nzStart;
-			if (num5 == num6)
+			int significandLength1 = end2 - nzStart2;
+			int significandLength2 = end1 - nzStart1;
+
+			if (significandLength1 == significandLength2)
 			{
-				int num7 = nzStart;
-				int num8 = nzStart2;
-				while (num7 <= i1)
+				int cursor1 = nzStart1;
+				int cursor2 = nzStart2;
+
+				while (cursor1 <= i1)
 				{
-					int num9 = s1[num7] - s2[num8];
-					if (num9 != 0)
-					{
-						return num9;
-					}
-					num7++;
-					num8++;
+					int digitDiff = s1[cursor1] - s2[cursor2];
+					if (digitDiff != 0)
+						return digitDiff;
+
+					cursor1++;
+					cursor2++;
 				}
-				num5 = end - num;
-				num6 = end2 - num2;
-				if (num5 == num6)
-				{
+
+                // Significands equal; use total length (including leading zeros) as tiebreaker
+                int totalLength1 = end1 - start1;
+                int totalLength2 = end2 - start2;
+
+                if (totalLength1 == totalLength2)
 					return 0;
-				}
-			}
-			if (num5 > num6)
-			{
-				return -1;
-			}
-			return 1;
-		}
+                if (totalLength1 > totalLength2)
+                    return -1;
+                return 1;
+            }
+
+            if (significandLength1 > significandLength2)
+                return -1;
+            return 1;
+        }
 
 		private static void ScanNumber(string s, int length, int start, ref int nzStart, ref int end)
 		{
 			nzStart = start;
 			end = start;
-			bool flag = true;
+			bool parsingLeadingZeros = true;
 			char c = s[end];
 			do
 			{
-				if (flag)
+				if (parsingLeadingZeros)
 				{
 					if ('0' == c)
 					{
@@ -234,7 +219,7 @@ namespace cYo.Common.Text
 					}
 					else
 					{
-						flag = false;
+						parsingLeadingZeros = false;
 					}
 				}
 				end++;
