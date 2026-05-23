2f745de8d50d1ea39c70712b8511f1309ef91dac
maforget <11904426+maforget@users.noreply.github.com>
2026-05-23 15:33:52 -0400

    Always enable creating ComicBook.xml on export
    For testing until we have added UI elements

--- ComicRack.Engine/IO/StorageSetting.cs
+++ ComicRack.Engine/IO/StorageSetting.cs
@@ -37,7 +37,7 @@ namespace cYo.Projects.ComicRack.Engine.IO
 			set;
 		}
 
-        [DefaultValue(false)]
+        [DefaultValue(true)]
         public bool EmbedComicBook
         {
             get;
@@ -175,7 +175,7 @@ namespace cYo.Projects.ComicRack.Engine.IO
 			RemovePages = true;
 			RemovePageFilter = ComicPageType.Deleted;
 			EmbedComicInfo = true;
-			EmbedComicBook = false;
+			EmbedComicBook = true;
             ComicCompression = ExportCompression.None;
 			ThumbnailSize = new Size(0, 256);
 			DoublePages = DoublePageHandling.Keep;
